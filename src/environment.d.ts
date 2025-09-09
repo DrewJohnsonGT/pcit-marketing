39659c8ef151f21406b7e8209e0df4403f359b5c
Drew Johnson <drewjohnsongt@gmail.com>
2025-09-09 14:53:26 -0400

    Running checks and installing deps to fix zod issue in lockfile

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,9 +1,9 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    DISCORD_WEBHOOK_URL: string;
     NEXT_PUBLIC_APP_URL: string;
     NEXT_PUBLIC_APP_VERSION: string;
     NEXT_PUBLIC_MARKETING_URL: string;
     NODE_ENV: string;
-    DISCORD_WEBHOOK_URL: string;
   }
 }
