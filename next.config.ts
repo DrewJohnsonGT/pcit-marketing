58e3b8a5e6c148a05761f678fc119e74a79ad417
Drew Johnson <drewjohnsongt@gmail.com>
2025-09-13 10:45:44 -0400

    Revert all multizone config changes

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,39 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // Avoids conflicting paths with the main app
-  assetPrefix: '/marketing',
+  assetPrefix: `${process.env.NEXT_PUBLIC_MARKETING_URL}`,
   devIndicators: false,
-  // async headers() {
-  //   // Allows the main app rewrites to access the marketing app resources
-  //   return [
-  //     {
-  //       headers: [
-  //         {
-  //           key: 'Access-Control-Allow-Origin',
-  //           value: process.env.NEXT_PUBLIC_APP_URL,
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Methods',
-  //           value: 'GET,OPTIONS',
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Credentials',
-  //           value: 'true',
-  //         },
-  //       ],
-  //       source: '/_next/static/:path*',
-  //     },
-  //   ];
-  // },
-  // // Re-writes all non-root "/" routes to the main app
-  // async rewrites() {
-  //   return [
-  //     {
-  //       destination: `${process.env.NEXT_PUBLIC_APP_URL}/:path*`,
-  //       source: '/:path*',
-  //     },
-  //   ];
-  // },
+  async headers() {
+    // Allows the main app rewrites to access the marketing app font files
+    return [
+      {
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NEXT_PUBLIC_APP_URL,
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+        source: '/_next/static/:path*',
+      },
+    ];
+  },
+  // Re-writes all non-root "/" routes to the main app
+  async rewrites() {
+    return [
+      {
+        destination: `${process.env.NEXT_PUBLIC_APP_URL}/:path*`,
+        source: '/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
