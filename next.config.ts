7e157cf2997d9d01855dabdb5ecb215d6febf463
Drew Johnson <drewjohnsongt@gmail.com>
2025-09-13 10:24:21 -0400

    Trying nextjs docs multizone setup

--- next.config.ts
+++ next.config.ts
@@ -2,30 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // Avoids conflicting paths with the main app
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/_marketing' : '',
+  assetPrefix: '/marketing',
   devIndicators: false,
-  async headers() {
-    // Allows the main app rewrites to access the marketing app resources
-    return [
-      {
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: process.env.NEXT_PUBLIC_APP_URL,
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-        ],
-        source: '/_next/static/:path*',
-      },
-    ];
-  },
   // Re-writes all non-root "/" routes to the main app
   async rewrites() {
     return [
