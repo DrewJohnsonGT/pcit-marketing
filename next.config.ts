fe6778868f5f5a87c3a70eb06818d1f030e4de87
Drew Johnson <drewjohnsongt@gmail.com>
2025-09-13 09:58:52 -0400

    Adding back header rules to allow cross site resource sharing between app and marketing site

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,28 @@ const nextConfig: NextConfig = {
   // Avoids conflicting paths with the main app
   assetPrefix: `${process.env.NEXT_PUBLIC_MARKETING_URL}`,
   devIndicators: false,
+  async headers() {
+    // Allows the main app rewrites to access the marketing app resources
+    return [
+      {
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NEXT_PUBLIC_APP_URL,
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+        source: '/_next/static/:path*',
+      },
+    ];
+  },
   // Re-writes all non-root "/" routes to the main app
   async rewrites() {
     return [
